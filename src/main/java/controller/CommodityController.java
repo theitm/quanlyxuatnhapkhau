0b63aa74711858bb7492d2d600e6fbb53ce2ccfb
nghia <ptn1905@gmail.com>
2021-12-15 15:17:13 +0700

    first commit

---
+++ src/main/java/controller/CommodityController.java
@@ -0,0 +1,57 @@
+package controller;
+
+import entity.Commodity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import service.CommodityService;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+
+@RestController
+public class CommodityController {
+    @Autowired
+    private CommodityService service;
+    @GetMapping("/commodity")
+    public List<Commodity> list() {
+        return service.listAll();
+    }
+    @GetMapping("/commodity/{id}")
+    public ResponseEntity<Commodity> get(@PathVariable UUID id) {
+        try {
+            Commodity commodity = service.get(id);
+            return new ResponseEntity<Commodity>(commodity, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<Commodity>(HttpStatus.NOT_FOUND);
+        }
+    }
+    List<Commodity> todoList = new CopyOnWriteArrayList<>();
+
+    @PostMapping("/commodity")
+    public ResponseEntity<Commodity> add(@RequestBody Commodity commodity) {
+        todoList.add(commodity);
+        service.save(commodity);
+        return ResponseEntity.ok().body(commodity);
+    }
+    @PutMapping("/tb_commodity/{id}")
+    public ResponseEntity<?> update(@RequestBody Commodity commodity, @PathVariable UUID id) {
+        try {
+            Commodity existProduct = service.get(id);
+            service.save(commodity);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @DeleteMapping("/commodity/{id} ")
+    public void delete(@PathVariable UUID id) {
+        service.delete(id);
+    }
+
+
+}
