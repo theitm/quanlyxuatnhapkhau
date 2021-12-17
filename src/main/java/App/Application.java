3833add73101dd070cfc336e2cdbb20745b61b79
nghia <ptnghia@gmail.com>
2021-12-17 10:25:21 +0700

    update

--- src/main/java/App/Application.java
+++ src/main/java/App/Application.java
@@ -1,6 +1,9 @@
+package App;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.testng.annotations.BeforeClass;
 
 @SpringBootApplication
 public class Application {
