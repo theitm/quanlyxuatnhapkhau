c851be50b445047a4f987190a778a04e211e29fc
nghia <ptnghia@gmail.com>
2022-01-19 14:17:33 +0700

    19/1

--- src/main/java/ImportExport/dto/detailsImportExport/DetailsImportExportBasicDto.java
+++ src/main/java/ImportExport/dto/detailsImportExport/DetailsImportExportBasicDto.java
@@ -5,13 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.io.Serializable;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 
-public class DetailsImportExportBasicDto {
+public class DetailsImportExportBasicDto implements Serializable {
     private Double quantity;
     private Double total;
     private CommodityBasicDto commodity;
--- src/main/java/ImportExport/dto/warehouseCommodity/WarehouseCommodityBasicDto.java
+++ src/main/java/ImportExport/dto/warehouseCommodity/WarehouseCommodityBasicDto.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 
-public class WarehouseCommodityBasicDto {
+public class WarehouseCommodityBasicDto implements Serializable {
     private double inventoryNumber;
 
 }
